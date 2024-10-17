6c7dbf88556b532be7847b133beaa6c9f160060c
Bladimir Holmann Zink <bladimirzink7@gmail.com>
2024-10-17 19:07:32 -0300

    adicion de fotos y blur hero

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,9 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), db()]
 });
